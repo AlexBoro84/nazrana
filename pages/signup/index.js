bcf1c13f594361addb8f3f88488bb96ccea8bf7b
Bablu Boro <babluboro8474@gmail.com>
2022-06-07 10:04:20 +0530

    Product Price Range Added

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useRouter } from 'next/router'
 import Footer from '../../components/Footer'
