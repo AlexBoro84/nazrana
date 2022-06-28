1e334de2f7fe886713a3edc1ddde9206c9084e79
AlexBoro84 <39063250+AlexBoro84@users.noreply.github.com>
2022-06-28 21:10:15 +0530

    Update index.js

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -75,13 +75,7 @@ const SignUp = () => {
                             <input id="password" name="password" type="password" onChange={(e) => setPassword(e.target.value)} value={password} required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"/>
                         </div>
                     </div>
-                    <div className="flex items-center justify-between mt-6">
-                        <div className="flex items-center">
-                            <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"/>
-                            <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900"> Remember me </label>
-                        </div>
-
-                    </div>
+            
                     <button onClick={handleSubmit} className="mt-8 w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         Sign up
                     </button>
